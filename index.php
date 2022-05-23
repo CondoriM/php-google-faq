7214106f9c9de378ce9e3a8ee93b82549734d0d1
CondoriM <condoria.miguel@gmail.com>
2022-05-23 16:13:49 +0200

    showed data

--- index.php
+++ index.php
@@ -34,3 +34,24 @@ $faqs =[
 ]
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google</title>
+</head>
+<body>
+
+    <?php foreach($faqs as $par) : ?>
+    <h1><?= $par["request"] ?></h1>
+    <?php foreach($par["result"] as $block) : ?>
+    <p><?= $block ?></p>
+    <?php endforeach ?>
+
+    <?php endforeach ?>
+
+</body>
+</html>         
